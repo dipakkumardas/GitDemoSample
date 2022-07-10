3679c0d84884820e976bd8d1f4d51fd3715c87bd
Dipak <das.dipak19@gmail.com>
2022-07-10 17:55:34 +0530

    added/updated/deleted

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -5,6 +5,9 @@ public class LoginPage {
 	public static void main(String a[])
 	{
 		System.out.println("Hello Login Page");
+		String loginname="admin";
+		String password = "admin";
+		System.out.println("The User Loggin");
 	}
 
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class SearchPage {
+	
+	public void Search()
+	{
+		System.out.println("This is the Search Method");
+	}
+
+}
